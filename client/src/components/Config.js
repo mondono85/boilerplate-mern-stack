f49cb02f082fdcd82378ce871f4745eb83de6784
mondono85 <mondono85@gmail.com>
2021-04-02 20:28:12 -0400

    done

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+export const API_KEY = '10447148ee3e7cd607b606fb7d567db4';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_URL= 'http://image.tmdb.org/t/p/'
+
+
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,58 @@
-import React from 'react'
+import React,{ useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
-
+import { API_URL, API_KEY, IMAGE_URL} from '../../Config'
+import {Typography, Row} from 'antd';
+import GridCard from './section/GridCard'
+const {Title} = Typography;
 function LandingPage() {
+    const [Movies, setMovies] = useState([])
+    const [CurrentPage, setCurrentPage] = useState(0)
+    useEffect(() => {
+        const endpoint =`${API_URL}movie/popular?api_key=${API_KEY}&language=en-us&page=1`
+        fetchMovies(endpoint)
+        
+    }, [])
+    
+    const fetchMovies = (path) => {
+        fetch(path)
+        .then(response=> response.json())
+        .then(response=>{
+            console.log(response)
+            setMovies([...Movies, ...response.results])
+            setCurrentPage(response.page)
+        })
+    }
+    const handleClick = () => {
+        let endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-us&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint);
+
+    }
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{width:'100%', margin:0}}>
+            <div style={{width: '85%', margin:'1rem auto'}}>
+                <Title level={2}>Movies by release date</Title>
+                <hr />
+                <Row gutter ={[16, 16]}>
+                    {Movies && Movies.map ((movie, index)=>(
+                        <React.Fragment key ={index}>
+                            <GridCard
+                             image={movie.poster_path && `${IMAGE_URL}w500${movie.poster_path}`}
+                             movieid={movie.id}
+                            
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+                <br/>
+                <div style={{display:'flex', justifyContent:'center'}}>
+                    <button onClick={handleClick}>load more</button>
+
+                </div>
+            
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+        </div>
     )
 }
 
